234f2b387ecb5c0807b064ae54093e5e26dab8ef
sohe1l <soheilansari@icloud.com>
2018-11-10 20:48:33 -0800

    Implemented simple login with MongoDB

---
+++ models/user.js
@@ -0,0 +1,66 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+
+var UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  name:{
+    first: String,
+    last: String
+  },
+  dob:{
+    type: Date
+  }
+});
+
+UserSchema.statics.authenticate = function (username, password, callback) {
+  User.findOne({ username: username })
+    .exec(function (err, user) {
+      if (err) {
+        return callback(err)
+      } else if (!user) {
+        var err = new Error('No user with the given username was found!');
+        err.status = 401;
+        return callback(err);
+      }
+      bcrypt.compare(password, user.password, function (err, result) {
+        if (result === true) {
+          return callback(null, user);
+        } else {
+        var err = new Error('Invalid password!');
+        err.status = 401;
+        return callback(err);
+        }
+      })
+    });
+}
+
+//hashing a password before saving it to the database
+UserSchema.pre('save', function (next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function (err, hash) {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  })
+});
+
+
+var User = mongoose.model('User', UserSchema);
+module.exports = User;
---
+++ routes/router.js
@@ -0,0 +1,110 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user');
+const path = require('path');
+
+// GET route for reading data
+router.get('/', function (req, res, next) {
+  return res.sendFile(path.join(__dirname + '/../template/index.html'));
+});
+
+
+//POST route for updating data
+router.post('/register', function (req, res, next) {
+  // confirm that user typed same password twice
+  if (req.body.password !== req.body.passwordConf) {
+    var err = new Error('Passwords do not match.');
+    err.status = 400;
+    res.send("passwords dont match");
+    return next(err);
+  }
+
+  if (req.body.email &&
+    req.body.username &&
+    req.body.password &&
+    req.body.firstname && 
+    req.body.lastname) {
+
+    var userData = {
+      email: req.body.email,
+      username: req.body.username,
+      password: req.body.password,
+      name: {
+        first: req.body.firstname,
+        last: req.body.lastname
+       }
+    }
+
+    User.create(userData, function (error, user) {
+      if (error) {
+        return next(error);
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('/profile');
+      }
+    });
+  } else {
+    var err = new Error('All fields required.');
+    err.status = 400;
+    return next(err);
+  }
+})
+
+
+router.post('/login', function (req, res, next) {
+  if (req.body.username && req.body.password) {
+    User.authenticate(req.body.username, req.body.password, function (error, user) {
+      if (error || !user) {
+        var err = new Error(error);
+        err.status = 401;
+        return next(err);
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('/profile');
+      }
+    });
+  } else {
+    var err = new Error('All fields required.');
+    err.status = 400;
+    return next(err);
+  }
+})
+
+
+
+
+// GET route after registering
+router.get('/profile', function (req, res, next) {
+  User.findById(req.session.userId)
+    .exec(function (error, user) {
+      if (error) {
+        return next(error);
+      } else {
+        if (user === null) {
+          var err = new Error('Not authorized! Go back!');
+          err.status = 400;
+          return next(err);
+        } else {
+          return res.send('<h1>Name: </h1>' + user.username + '<h2>Mail: </h2>' + user.email + '<br><a type="button" href="/logout">Logout</a>')
+        }
+      }
+    });
+});
+
+// GET for logout logout
+router.get('/logout', function (req, res, next) {
+  if (req.session) {
+    // delete session object
+    req.session.destroy(function (err) {
+      if (err) {
+        return next(err);
+      } else {
+        return res.redirect('/');
+      }
+    });
+  }
+});
+
+
+
+module.exports = router;
